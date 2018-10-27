64052855124677e4793791f4779596978c0edb74
rasika <rasikapohankar@gmail.com>
2018-10-27 02:10:05 -0400

    Revert "Read file path from command line"
    This reverts commit f65c5fef0fcc56d7c2b8ac2f953c992dc5fc81ac.

--- make_dirs.sh
+++ make_dirs.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-dir=$1
-op_path=$2
+dir="../matrices"
+op_path="../alv_matrices"
 
-for file in "${dir}"/*; do
+for file in "$dir"/*; do
 	name=${file%.*}
 	ext=${file##*.}
 	if [ $ext != "gz" ]; then
 		name=${name%_*}
+		#dir_name=${dir_name##*/}
+		#mkdir -p $op_path/$dir_name
+		#cp $file $op_path/$dir_name
 	fi
 	dir_name=${name##*/}
-        mkdir -p "${op_path}"/$dir_name
-        cp "${file}" "${op_path}"/$dir_name
+        mkdir -p $op_path/$dir_name
+        cp $file $op_path/$dir_name
 done
