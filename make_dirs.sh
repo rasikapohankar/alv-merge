18b59cc6100f3dfba0fb5913b85b1867c695b381
rasika <rasikapohankar@gmail.com>
2018-10-27 02:21:58 -0400

    Remove comments from bash script

--- make_dirs.sh
+++ make_dirs.sh
@@ -3,16 +3,13 @@
 dir=$1
 op_path=$2
 
-for file in "$dir"/*; do
-	name=${file%.*}
-	ext=${file##*.}
-	if [ $ext != "gz" ]; then
-		name=${name%_*}
-		#dir_name=${dir_name##*/}
-		#mkdir -p $op_path/$dir_name
-		#cp $file $op_path/$dir_name
-	fi
-	dir_name=${name##*/}
-        mkdir -p $op_path/$dir_name
-        cp $file $op_path/$dir_name
+for file in "${dir}"/*; do
+        name=${file%.*}
+        ext=${file##*.}
+        if [ $ext != "gz" ]; then
+                name=${name%_*}
+        fi
+        dir_name=${name##*/}
+        mkdir -p "${op_path}"/$dir_name
+        cp "${file}" "${op_path}"/$dir_name
 done
