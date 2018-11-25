647834e4749699b13c0f42f8d1c6926e1a8fe5f3
rasika <rasikapohankar@gmail.com>
2018-11-24 22:13:12 -0500

    Clean normalization cod

--- cpp/normalize_matrices.cpp
+++ cpp/normalize_matrices.cpp
@@ -15,14 +15,14 @@
 #include <chrono>
 
 using namespace std;
+namespace io=boost::iostreams; 
+namespace fs=boost::filesystem;
 
 int read_row_file(string path, unordered_map<string, int>& map) {
         string line;
         ifstream infile(path);
-        int count = 0;
 
         while (getline(infile, line)) {
-                count++;
                 if (map.count(line) > 0) {
                         map.at(line) = map.at(line) + 1;
                 } else {
@@ -30,16 +30,21 @@ int read_row_file(string path, unordered_map<string, int>& map) {
                 }
         }
         infile.close();
-        return count;
+        return 0;
 }
 
-unordered_map<string, int> get_cb_counts(boost::filesystem::path path, int num_matrices) {
+string get_path(fs::path base, string dir, string suffix) {
+        base = base / dir / dir;
+        base += suffix;
+        return base.string();
+}
+
+unordered_map<string, int> get_cb_counts(fs::path path) {
 	vector<string> files = vector<string>();
 	DIR *dp;
 	struct dirent *dirp;
 	unordered_map<string, int> map;
-	boost::filesystem::path base_path;
-	int count = 0;
+	fs::path base_path;
 	if ((dp = opendir(path.c_str())) == NULL) {
 		cout << "Error opening dir\n";
 	//	return;
@@ -49,14 +54,9 @@ unordered_map<string, int> get_cb_counts(boost::filesystem::path path, int num_m
 		if (strcmp(dirp->d_name, ".") != 0 && 
 			strcmp(dirp->d_name, "..") != 0) {
 			base_path = path;
-			base_path = base_path / dirp->d_name / dirp->d_name;
-			base_path += "_rows.txt";
-//			cout << base_path.string() << endl;
-			int count = read_row_file(base_path.string(), map);
-			count++;
+			string file_path = get_path(base_path, dirp->d_name, "_rows.txt");
+			read_row_file(file_path, map);
 		}
-		if (count == num_matrices)
-			break;
 	}
 	closedir(dp);
 	return map;
@@ -64,90 +64,96 @@ unordered_map<string, int> get_cb_counts(boost::filesystem::path path, int num_m
 
 int read_files(string matrix_path, vector<vector<double>>& cell_counts)
 {
-	time_t start, end;
-	int num_genes = 52325; //TO-DO: read from _cols file
+        int num_genes = 52325; //TO-DO: read from _cols file
         int count = 0;
 
-	time(&start);
+	auto start = std::chrono::high_resolution_clock::now();
 
-	// open matrix file
-	boost::iostreams::filtering_istream matrix_stream;
-	matrix_stream.push(boost::iostreams::gzip_decompressor());
-	matrix_stream.push(boost::iostreams::file_source(matrix_path,
-							 ios_base::in | ios_base::binary));
+        // open matrix file
+        io::filtering_istream matrix_stream;
+        matrix_stream.push(io::gzip_decompressor());
+        matrix_stream.push(io::file_source(matrix_path, 
+						ios_base::in | ios_base::binary));
 
-	size_t elem_size = sizeof(typename vector<double>::value_type);
-	vector<double> num_vector (num_genes, 0.0);
-	double sum = 0.0;
-	int c = 0;
-	while (matrix_stream.read(reinterpret_cast<char *>(num_vector.data()), elem_size * num_genes)) {
-		for (std::vector<double>::iterator it = num_vector.begin() ; it != num_vector.end(); ++it) {
-			if (c < 10)
-				cout << *it << endl;
-			sum += *it;
-			c++;		
-	        }
-		cell_counts.push_back(num_vector);
-	}
-	time(&end);
-	cout << "Time for file " << matrix_path << ": " << end - start << " sec" << endl;
-	return sum;
+        size_t elem_size = sizeof(typename vector<double>::value_type);
+        vector<double> num_vector (num_genes, 0.0);
+        double sum = 0.0;
+        while (matrix_stream.read(reinterpret_cast<char *>(num_vector.data()), 
+								elem_size * num_genes)) {
+                for (vector<double>::iterator it = num_vector.begin(); it != num_vector.end(); ++it) {
+                        sum += *it;
+                }
+                cell_counts.push_back(num_vector);
+        }
+	auto end = std::chrono::high_resolution_clock::now();
+        cout << "Time for file: " << matrix_path << " "
+             << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+             << " ms" << endl;
+        return sum;
 }
 
+void open_stream(fs::path output_path,
+			io::filtering_ostream& op_stream) {
+        op_stream.push(io::gzip_compressor());
+        op_stream.push(io::file_sink(output_path.string(),
+                                ios_base::out | ios_base::binary));
+}
 
-int concat_matrix(boost::filesystem::path path,
-		  boost::filesystem::path& output_path,
-		  unordered_map<string, int>& cb_counts,
-		  int num_matrices
-		) {
+void write_cols(fs::path path, string ip_path) {
+	ofstream out_cols;
+	string gene;
+        
+	out_cols.open(path.string());
+        ifstream col_file(ip_path, ios_base::in);
+        while (getline(col_file, gene)) {
+                out_cols << gene << "\n";
+        }
+        out_cols.close();
+}
+
+void normalize(unordered_map<string, vector<vector<double>>> cell_counts, int max,
+		fs::path output_path) {
+	size_t elem_size = sizeof(typename vector<double>::value_type);
+	int num_genes = 52325;
+        for (auto item : cell_counts) {
+                for (auto row: item.second) {
+                        std::transform(row.begin(), row.end(), row.begin(),
+                        [&max](double c) -> double { return c / max; });
+                }       
+                auto matrix_out_path = output_path / item.first;
+                matrix_out_path += ".gz";
+
+		io::filtering_ostream op_stream;
+		auto path = output_path / item.first;
+		path += ".gz";
+	        open_stream(path, op_stream);
+                op_stream.write(reinterpret_cast<char *>((item.second).data()),
+					elem_size * num_genes * (item.second).size());
+        }
+}
+
+int concat_matrix(fs::path path, fs::path& output_path,
+		  unordered_map<string, int>& cb_counts) {
         DIR *dp;
         struct dirent *dirp;
-	boost::filesystem::path base_path;
-	int count = 0;
-	double max = 0.0, sum = 0.0;
-	int num_genes = 52325;
+	fs::path base_path;
+	double sum = 0.0,
+	       max = 0.0;
 	unordered_map<string, vector<vector<double>>> cell_counts;
-	cout << "dir in concat matrix " << path.string() << endl;
 
         if ((dp = opendir(path.c_str())) == NULL) {
                 cout << "Error opening dir\n";
-        //      return;
+		return -1;
         }
 
-	// open output matrix file
-        auto matrix_out_path = output_path / "out_matrix.gz";
-	boost::iostreams::filtering_ostream op_matrix_stream;
-	op_matrix_stream.push(boost::iostreams::gzip_compressor());
-	op_matrix_stream.push(boost::iostreams::file_sink(matrix_out_path.string(),
-								ios_base::out | ios_base::binary));
-
-	std::filebuf op_matrix;
-	op_matrix.open(matrix_out_path.string(), std::ios::out);
-	std::ostream op_file(&op_matrix);
-
-	// open output rows file
-        ofstream out_rows, out_cols;
-
-        auto rows_path = output_path / "out_rows.txt";
-        out_rows.open(rows_path.string());
-
-	string cols_file;
+	string cols_path;
         while ((dirp = readdir(dp)) != NULL) {
 		if (strcmp(dirp->d_name, ".") != 0 && strcmp(dirp->d_name, "..") != 0) {
 			base_path = path;
-			base_path = base_path / dirp->d_name / dirp->d_name;
-			base_path += ".gz";
-			string matrix_path = base_path.string();
 
-			base_path.remove_leaf();
-			base_path = base_path / dirp->d_name;
-			base_path += "_rows.txt";
-			string rows_file = base_path.string();
-
-			base_path.remove_leaf();
-                        base_path = base_path / dirp->d_name;
-                        base_path += "_cols.txt";
-                        cols_file = base_path.string();
+			string matrix_path = get_path(base_path, dirp->d_name, ".gz");
+			string rows_path = get_path(base_path, dirp->d_name, "_rows.txt");
+			cols_path = get_path(base_path, dirp->d_name, "_cols.txt");
 
 			vector<vector<double>> matrix_count;
 			sum = read_files(matrix_path, matrix_count);
@@ -157,113 +163,31 @@ int concat_matrix(boost::filesystem::path path,
 				max = sum;
                 }
 	}
-	cout << "max sum is " << max << endl;
-	size_t elem_size = sizeof(typename vector<double>::value_type);
-	int k = 0;
-	for (auto item : cell_counts) {
-		for (auto row: item.second) {
-			if (k < 10) {
-				cout << "before: " << endl;
-				for (int i = 0; i < 10; i++)
-					cout << row[i] << "  ";
-			}
-			std::transform(row.begin(), row.end(), row.begin(),
-			[&max](double c) -> double { return c / max; });
-			if (k < 10) {
-				cout << "after: " << endl;
-				for (int i = 0; i < 10; i++)
-                	                cout << row[i] << "  ";
-			}
-			k++;
-//			std::bind(std::divides<T>(), std::placeholder::_1, max));
-		}
-		cout << "item first is: " << item.first << endl;
-		auto matrix_out_path = output_path / item.first;
-		matrix_out_path += ".gz";
-		boost::iostreams::filtering_ostream op_matrix_stream;
-		op_matrix_stream.push(boost::iostreams::gzip_compressor());
-		op_matrix_stream.push(boost::iostreams::file_sink(matrix_out_path.string(),
-                                                                ios_base::out | ios_base::binary));
-		op_matrix_stream.write(reinterpret_cast<char *>((item.second).data()), elem_size * num_genes * (item.second).size());
-	}
-
+	normalize(cell_counts, max, output_path);
         closedir(dp);
-	cout << "closed dir " << endl;
-	cout << "closed out reads file" << endl;
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
 	string s_path = argv[1];
 	string s_output_path = argv[2];
-	int num_matrices = std::stoi(argv[3]);
-	time_t st, et;
-	boost::filesystem::path in_path{s_path};
-	boost::filesystem::path out_path{s_output_path};
-	
+
+	fs::path in_path{s_path};
+	fs::path out_path{s_output_path};
+
 	cout << "Getting cb counts" << endl;
         auto start = std::chrono::high_resolution_clock::now();
-        unordered_map<string, int> cb_counts = get_cb_counts(in_path, num_matrices);
+        unordered_map<string, int> cb_counts = get_cb_counts(in_path);
         auto end = std::chrono::high_resolution_clock::now();
-        cout << "Got cb counts" << endl;
-        cout << "Time " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
+        cout << "CB counts time " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << endl;
 
 	cout << "Starting concatenation.." << endl;
-	time(&st);
 	auto s = std::chrono::high_resolution_clock::now();
-	concat_matrix(in_path, out_path, cb_counts, num_matrices);
-	time(&et);
-	cout << "returned from concat_matrix" << endl;
+	int ret = concat_matrix(in_path, out_path, cb_counts);
 	auto e = std::chrono::high_resolution_clock::now();
 	cout << "Time to concatenate: " << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count() << " ms" << endl;
-	cout << "Time with time: " << et - st << endl;
 	cout << "exiting.." << endl;
 
-	/*for (auto &x: cb_counts) {
-		if (x.second > 0)
-	        	cout << x.first << ":" << x.second << endl;
-       	}*/
 	return 0;
 }
 
-// These functions are not currently used
-/*
-int write_non_unique(unordered_map<std::string, vector<vector<double>>>& cell_counts,
-                     boost::iostreams::filtering_ostream& op_matrix_stream,
-                     ofstream& out_reads) {
-        int count = 0;
-        // write cell counts of repeated reads
-        for (auto content: cell_counts) {
-                for(auto &cell: content.second) {
-                        out_reads << content.first << "_" << count << "\n";
-                        op_matrix_stream.write(reinterpret_cast<char *>(num_vector.data()),
-                                                                         elem_size * num_genes);
-                        count++;
-                }
-        }
-        return 0;
-}
-
-int write_unique(string s_read,
-                     unordered_map<string, int> cb_counts,
-                     unordered_map<std::string, vector<std::vector<double>>>& cell_counts,
-                     boost::iostreams::filtering_ostream& op_matrix_stream,
-                     ofstream& out_reads) {
-
-        if (cb_counts.at(s_read) == 1) { // write to output file
-                op_matrix_stream.write(reinterpret_cast<char *>(num_vector.data()), elem_size * num_genes);
-                out_reads << s_read << "\n";
-        } else { // keep in memory
-                vector<vector<double>> nums;
-                if (cell_counts.count(s_read) > 0) {
-                        nums = cell_counts.at(s_read);
-                        nums.push_back(num_vector);
-                        cell_counts.at(s_read) = nums;
-                } else {
-                        nums.push_back(num_vector);
-                        cell_counts.insert({s_read, nums});
-                }
-        }
-        return 0;
-}
-*/
