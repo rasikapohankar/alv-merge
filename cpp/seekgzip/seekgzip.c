4c05245c334b5669de8bcfcddc4224efd538a54f
rasika <rasikapohankar@gmail.com>
2018-12-05 21:14:10 -0500

    Remove unused parts from seekgzip.c

--- cpp/seekgzip/seekgzip.c
+++ cpp/seekgzip/seekgzip.c
@@ -575,7 +575,7 @@ seekgzip_t* seekgzip_open(const char *target, int *errorcode)
         gzread(gz, zs->index.list[i].window, WINSIZE);
     }
 
-    // Close the index filiiiie.
+    // Close the index file.
     if (gzclose(gz) != 0) {
         ret = SEEKGZIP_ZLIBERROR;
         goto error_exit;
@@ -681,84 +681,6 @@ static void seekgzip_perror(int ret)
     case SEEKGZIP_ZLIBERROR:
         fprintf(stderr, "ERROR: An error occurred in zlib.\n");
         break;
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    int ret = 0;
-
-    if (argc != 3) {
-        printf("This utility manages an index for random (seekable) access to a gzip file.\n");
-        printf("USAGE:\n");
-        printf("    %s -b <FILE>\n", argv[0]);
-        printf("        Build an index file \"$FILE.idx\" for the gzip file $FILE.\n");
-        printf("    %s <FILE> [BEGIN-END]\n", argv[0]);
-        printf("        Output the content of the gzip file $FILE of offset range [BEGIN:END).\n");
-        return 0;
-
-    } else if (strcmp(argv[1], "-b") == 0) {
-        const char *target = argv[2];
-
-        printf("Building an index: %s.idx\n", target);
-        printf("Filesize up to: %d bit\n", (int)sizeof(off_t) * 8);
-
-        ret = seekgzip_build(target);
-        if (ret != 0) {
-            seekgzip_perror(ret);
-            return 1;
-        }
-        return 0;
-
-    } else {
-        char *arg = argv[2], *p = NULL;
-        off_t begin = 0, end = (off_t)-1;
-        seekgzip_t* zs = seekgzip_open(argv[1], NULL);
-        if (zs == NULL) {
-            fprintf(stderr, "ERROR: Failed to open the index file.\n");
-            return 1;
-        }
-
-        p = strchr(arg, '-');
-        if (p == NULL) {
-            begin =(off_t)strtoull(arg, NULL, 10);
-            end = begin+1;
-        } else if (p == arg) {
-            begin = 0;
-            end = (off_t)strtoull(p+1, NULL, 10);
-        } else if (p == arg + strlen(arg) - 1) {
-            *p = 0;
-            begin = (off_t)strtoull(arg, NULL, 10);
-        } else {
-            *p++ = 0;
-            begin =(off_t)strtoull(arg, NULL, 10);
-            end =(off_t)strtoull(p, NULL, 10);
-        }
-
-        seekgzip_seek(zs, begin);
-
-        while (begin < end) {
-            int read;
-            char buffer[CHUNK];
-            off_t size = (end - begin);
-            if (CHUNK < size) {
-                size = CHUNK;
-            }
-            read = seekgzip_read(zs, buffer, (int)size);
-            if (0 < read) {
-                fwrite(buffer, read, sizeof(char), stdout);
-                begin += read;
-            } else if (read == 0) {
-                break;
-            } else {
-                fprintf(stderr, "ERROR: An error occurred while reading the gzip file.\n");
-                ret = 1;
-                break;
-            }
-        }
-    
-        seekgzip_close(zs);
-        return ret;
     }
 }
 
